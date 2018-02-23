a2f85d39255f9ac653ecb6e23e1f93d09e940dcd
Dmitry <dimanbotandiman@mail.ru>
2018-02-23 15:13:06 +0400

    java project core was created.

---
+++ src/src/Main.java
@@ -0,0 +1,10 @@
+import  utils.Utils;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Utils.log("Lol");
+
+    }
+}
---
+++ src/src/utils/Utils.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class Utils {
+
+    public static void log(String args) {
+        System.out.println(args);
+    }
+}
