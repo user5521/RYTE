1ed64bf7c042521f4330faaf8761d90ac818eea0
Dmitry <dimanbotandiman@mail.ru>
2018-02-24 18:55:43 +0400

    class controller was added.

---
+++ src/src/controller/Controller.java
@@ -0,0 +1,18 @@
+package controller;
+
+public class Controller {
+
+    public static void add(){
+
+    }
+    public static void del(){
+
+    }
+    public static void abdate(){
+
+    }
+    public static void select(){
+
+    }
+
+}
